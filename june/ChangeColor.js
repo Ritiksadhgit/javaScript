c81b07f5e86cbdd8e9fe1a0e2782aa8bfe22c631
Ritik <sadhritik6@gmail.com>
2026-08-24 21:32:22 +0530

    basic code of js

---
+++ june/ChangeColor.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    alert("I am Ritik");
+}
+// myFunction();
+
+function myFunction2() {
+    var x= confirm("Are you sure want to delete");
+    console.log(x);
+}
+// myFunction2();
+
+function changecolor() {
+    var x=document.getElementById("para");
+    x.style.color="red";
+    console.log(x);
+}

b702ab7b5cb708afb74335c27aa5e8830a8022fb
Ritik <sadhritik6@gmail.com>
2026-08-25 23:58:04 +0530

    For loop

---
+++ june/kasimSir/ForLoop.js
@@ -0,0 +1,38 @@
+
+
+// For loop
+
+// for(initialization; condition; inc/dec) {
+//     code
+// }
+
+
+
+// let num = parseInt(prompt("Enter a Number"));
+
+// if (num % 3 == 0 && num % 5 == 0) {
+
+//     for (let i = 10; i >= 1; i--) {
+//         document.write(num + " x " + i + " = " + (num * i) + "<br>");
+//     }
+
+// } else {
+//     document.write("Square = " + (num * num));
+// }
+
+
+let sum = 0;
+for (let i= 1; i<= 15;i++) {
+    if (i%4== 0) {
+        sum=sum + i;
+    }
+}
+console.log(sum);
+
+
+let a=parseInt(prompt("Enter1"));
+let b=parseInt(prompt("Enter2"));
+
+if(b%2!=0) {
+
+}
